cbca1fa9883b007bd1ad3ff9f0aa95c95cca5f90
TrollFacedGamer <bwang@brooklynsteamcenter.org>
2024-12-09 10:52:12 -0500

    12/9/2024

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 from flask import Flask, render_template
+import pymysql
+# classses are capitalized
+from dynaconf import Dynaconf
 
 app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
+
+
+def connect_db():
+    conn =pymysql.connect(
+        host="10.100.34.80",
+        database="bwang_streamline_water",
+        user='bwang',
+        password= conf.password,
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor
+    )
+
+    return conn
 
 #coordinator connect two fuction
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def product_browse():
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM `Product` ;")
+
+    results = cursor.fetchall()
+
+
+    return render_template("browse.html.jinja", products = results)
+
+
+
+
