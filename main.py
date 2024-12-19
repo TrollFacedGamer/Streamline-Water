a3c06eb0329a0073142b9829a19319815c93d7a6
TrollFacedGamer <bwang@brooklynsteamcenter.org>
2024-12-19 10:50:30 -0500

    login

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, abort
 import pymysql
 # classses are capitalized
 from dynaconf import Dynaconf
+import flask_login
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ conf = Dynaconf(
 )
 
 app.secret_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = ('/sign_in')
+
 
 def connect_db():
     conn =pymysql.connect(
@@ -22,6 +28,36 @@ def connect_db():
     )
 
     return conn
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, email, first_name, last_name):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id}")
+
+    result = cursor.fetchone()
+    #if there is no vaule in the requested database vaule a None will be returned
+    cursor.close()
+    conn.close()
+
+    if result is not None:
+        return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
 
 #coordinator connect two fuction
 @app.route("/")
@@ -66,11 +102,20 @@ def product_page(product_id):
     conn.close()
     # used to not DDOX the database
 
+    if result is None:
+        abort(404)
+        #redirect to 404 page
     return render_template("product.html.jinja", product = result)
-    # if you return with a string the page will just have that string
+    # if you return with a string the page will just have that string 
+    
+
+    
 
 @app.route("/sign_up", methods=["POST", "GET"])
 def sign_up_page():
+    if flask_login.current_user.is_authenticated:
+        return redirect("/")
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -109,29 +154,50 @@ def sign_up_page():
             flash("Sorry, Your Password need to be stronger: it should be at least 10 characters long")
     return render_template("sign_up.html.jinja")
 
-@app.route("/sign_in")
+@app.route("/sign_in", methods=["POST", "GET"])
 def sign_in_page():
+    if flask_login.current_user.is_authenticated:
+        return redirect("/")
+
+
     if request.method == "POST":
-        username = request.form["username"]
+        username = request.form["username"].strip()
         password = request.form["password"]
         
         conn = connect_db()
         cursor = conn.cursor()
 
 
-        cursor.execute(f"SELECT * FROM `Product` WHERE `username` = {username};")
+        cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}' ;")
+        #there are '' around {username} because its a string
 
-        account = cursor.fetchone()
-        
-        if password == account["password"]:
-            print("login")
-            #not done
+        result = cursor.fetchone()
+
+        if result is None:
+            flash("Your username/password is incorrect")
+
+        elif password != result["password"]:
+            flash("Your username/password is incorrect")
+
         else:
-            flash("Sorry, Your Password is incorrect")
+            user = User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+            flask_login.login_user(user)
+
+            return redirect('/')
 
         cursor.close()
         conn.close()
 
     return render_template("sign_in.html.jinja")
 
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
+
 #refreshing a form error cause it to send the form again
+
+@app.route("/cart")
+@flask_login.login_required
+def cart():
+    return "cart page"
